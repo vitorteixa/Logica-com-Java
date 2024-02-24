922c5dd205e0054d8a9488a87f3ba11c67b41a39
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-02-24 01:45:26 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+class Main {
+	public static void main(String[] args) {
+		// introdução às variáveis em java
+
+		String nome = "Vitor T Santos";
+
+		System.out.println(nome.contains("Vitor")); // verifica se existe dentro da string
+		System.out.println(nome.length()); // conta o número de caracteres
+	}
+
+}
