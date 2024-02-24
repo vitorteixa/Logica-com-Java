f574cd5a254334931d862621424301a9dbde4d4d
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-02-24 01:51:09 +0000

    Aula 04 de 6  entrada e saída

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ class Main {
 		// introdução às variáveis em java
 
 		String nome = "Vitor T Santos";
-
 		System.out.println(nome.contains("Vitor")); // verifica se existe dentro da string
 		System.out.println(nome.length()); // conta o número de caracteres
+		System.out.println(""); // pula linha após a execução
+		System.out.print(""); // não pula linha após a execução
+		System.out.printf(""); // não pula linha após a execução
 	}
 
 }
