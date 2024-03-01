ad98ba81d77e09104bdad2262894db0142e1fd84
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-03-01 19:22:08 +0000

    Estrutura condicional if

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,22 @@
 
 public class Main {
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
+		// ESTRUTURA CONDICIONAL SIMPLES
 		int nota1 = 6;
-		int nota2 = 7;
-		
-		if (nota1 >= 5) {
-			System.out.println("aprovado");
-			
-		}
-  }
+		int nota2= 7;
 
-	
+		if (nota1 >= 5) {
+			System.out.println("nota1: aprovado");
+
+		}
+		// ESTRUTURA CONDICIONAL COMPOSTA
+
+if(nota2 >= 5)
+{
+    System.out.println("nota2: aprovado");
 }
+else {
+    System.out.println("nota2: reprovado");
+}
+	}
+	}
