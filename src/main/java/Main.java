75ec87f004d70b670365aff110ad31e16073dbd7
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-03-04 23:18:31 +0000

    04-03-2024

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,22 +3,21 @@ public class Main {
 	public static void main(String[] args) {
 		// ESTRUTURA CONDICIONAL SIMPLES
 		int nota1 = 6;
-		int nota2= 7;
+		int nota2 = 7;
 
 		if (nota1 >= 5) {
 			System.out.println("nota1: aprovado");
 
-		};
+		}
+		;
 		System.out.println("teste Versionamento");
-		
+
 		// ESTRUTURA CONDICIONAL COMPOSTA
 
-if(nota2 >= 5)
-{
-    System.out.println("nota2: aprovado");
-}
-else {
-    System.out.println("nota2: reprovado");
-}
+		if (nota2 >= 5) {
+			System.out.println("nota2: aprovado");
+		} else {
+			System.out.println("nota2: reprovado");
+		}
 	}
-	}
+
