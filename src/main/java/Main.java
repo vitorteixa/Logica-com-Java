a2f9918cf9497ee4dafebffd097580e2c38a2a3c
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-03-04 23:30:56 +0000

    teste 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,9 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-// import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+		int x = 1;
   }
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
-}
+  
