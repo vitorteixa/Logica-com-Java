520639f0cd217024686ff822394f17c20d1eea78
Vitor Santos <12648403-vitorteixa@users.noreply.replit.com>
2024-03-05 00:44:32 +0000

    var2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 	public static void main(String[] args) {
-    System.out.println("Hello world!");
+		System.out.println("Hello world!");
 		int x = 1;
-  }
+		int y = 2;
+	}
 }
