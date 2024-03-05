dd7b6f5fe242c7a57923902c95eea16a1627096e
Vitor Santos <12648403-vitorteixa@users.noreply.replit.com>
2024-03-05 02:05:33 +0000

    agora vai

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,11 @@
 import java.util.Scanner;
 
-
 public class Main {
-  public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
 		System.out.println("Escolha uma opção: ");
-		System.out.println("1 -  Cadastrar aluno: ");
-		System.out.println("2 -  Cadastrar notas: ");
-		System.out.println("3 -  Listar alunos e notas: ");
-
-		int numero = entrada.nextInt();
-		switch(numero){
-				case 1:
-				System.out.println("Cadastro de aluno");
-				break;
-			 case 2: 
-				System.out.println("Cadastro de notas");
-				break;
-			case 3: 
-				System.out.println("Listar alunos e notas");
-				break;
-			default: 
-				System.out.println("Opção a");
-		}
 		
-  }
-	
-	
- 
+
+	}
+
 }
