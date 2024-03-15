bb6fe484ac76027e25100950a6d0e94d662bcd2b
vitorteixa <33202529+vitorteixa@users.noreply.github.com>
2024-03-15 01:34:54 +0000

    alguns exercicios

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,46 +2,48 @@ import java.util.Scanner;
 
 class Main {
 
-	public static void main(String[] args) {
-		// ESTRUTURA DE REPETIÇÃO: while
-
-		Scanner entrada = new Scanner(System.in);
-		int opcao = 0;
-		while (opcao != 99) {
-			System.out.println("Digite um numero qualquer, ou o 99 para sair: ");
-			opcao = entrada.nextInt();
-		}
-		;
-		// ESTRUTURA DE REPETIÇÃO: do/while
-
-		int opcao2;
-		do {
-			System.out.println("Digite um valor ou 99 para sair: ");
-			Scanner entrada2 = new Scanner(System.in);
-			opcao2 = entrada2.nextInt();
-		} while (opcao2 != 99);
-
-		// ESTRUTURA DE REPETIÇÃO: for
-		// ++ = incremento
-		// -- = decremento
-		for (int cont = 0; cont <= 10; cont++) {
-
-			System.out.println("Valor do contador: " + cont);
-		}
-
-		for (int cont2 = 5; cont2 <= 10; cont2++) {
-			System.out.println("Valor do contador de 5 a 10: " + cont2);
-		}
-		;
-
-		// ESTRUTURA DE REPETIÇÃO: aninhada
-		// i = esquerda
-		// j = direita
-		for (int i = 0; i <= 10; i++)
-			for (int j = 0; j <= 10; j++)
-				System.out.println(j + " X " + i + "=" + (i * j));
-		System.out.println("----------------------");
-
+	public static int somar(int numero1, int numero2){
+		return (numero1 + numero2);
 	}
+	
+	public static void main(String[] args) {
+	 
+		int total = Main.somar(1,2);
+		System.out.println(total);
+		for(int x=1; x<10; x++)
+		{
+							if(x%4 !=0 && x%2 == 0)
+							{
+												for(int y=1; y<5; y++) 
+												{
+																	System.out.println(y);
+												}
+							}
+		};
+		int d,x;
+		d= 0;
+		x = 314;
+		while(x>1){
+			x=x/2;d=d+1;
+		}
+		System.out.println("d = "+d);
+		System.out.println("x = "+x);
+/*
+Considere criar um programa que seja responsável por receber o nome dos cinco melhores filmes ou séries que você já assistiu. Observe que para cada filme você necessitará de uma variável diferente para armazenar os valores na memória do computador. Após digitar o nome dos filmes ou séries, experimente mostrá-los na tela. Importante: somente com a prática você obterá a proficiência em programação.
+*/
+		Scanner entrada = new Scanner(System.in);
+		String[] filmesSeries = new String[5];
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Digite o nome do " + (i + 1) + "º filme ou série: ");
+			filmesSeries[i] = entrada.nextLine();
+		}
 
+		System.out.println("Filmes ou séries que você já assistiu:");
+		for (String filmeSerie : filmesSeries) {
+			System.out.println(filmeSerie);
+		}
+       
+	}
 }
+
+
